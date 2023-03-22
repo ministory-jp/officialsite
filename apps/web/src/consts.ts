6b84d25d73581839bb0d46766fee28f786e5cb8e
Daiki Shiozawa <me@nacl30d.com>
2023-03-22 22:53:06 +0900

    :sparkles: Implement first view

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ミニストーリー';
+export const SITE_DESCRIPTION = `ミニストーリーは
+「見た人の心が動かされる瞬間を作りたい」
+「クリスチャンが喜び仕える機会を作りたい」
+という2つの目的のもとに2022年5月に始まった、
+出張パフォーマンスミニストリーです`;
+
+export const SOCIAL_LINK = {
+    instagram: 'https://instagram.com/ministory0909',
+    facebook: 'https://www.facebook.com/ミニストーリー-109870978699769/',
+};
