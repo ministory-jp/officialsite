51ef8ed4f3a6977cbef52d9066251b0e050dc8e7
Daiki Shiozawa <me@nacl30d.com>
2023-03-23 14:43:07 +0900

    :gem: Update tone and manner

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ミニストーリー';
-export const SITE_DESCRIPTION = `ミニストーリーは
+export const SITE_TITLE = 'ミニ・ストーリー';
+export const SITE_DESCRIPTION = `ミニ・ストーリーは
 「見た人の心が動かされる瞬間を作りたい」
 「クリスチャンが喜び仕える機会を作りたい」
 という2つの目的のもとに2022年5月に始まった、
