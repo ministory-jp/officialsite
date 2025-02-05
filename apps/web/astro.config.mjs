94a47ee4db32510f8a1d047fb6191287c7e2258c
Daiki Shiozawa <me@nacl30d.com>
2025-02-05 10:58:34 +0900

    chore: Upgrade tailwindcss

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.ministory.jp',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap()],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
